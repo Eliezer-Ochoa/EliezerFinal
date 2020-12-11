68e98de0378e177178938464d5d21cf90148b8f7
Eliezer Ochoa <eliezerochoa20@gmail.com>
2020-12-11 19:59:41 +0100

    Agregue una imagen como boton para acceder a la segunda tambien cree el metodo onclick en em MainActivity y en la Imagen

--- FINAL/app/src/main/java/com/eliezer/afinal/MainActivity.kt
+++ FINAL/app/src/main/java/com/eliezer/afinal/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.eliezer.afinal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun AccederRegistro(view: View) {
+        val intent = Intent(this, Registro::class.java)
+        startActivity(intent)
     }
 }
