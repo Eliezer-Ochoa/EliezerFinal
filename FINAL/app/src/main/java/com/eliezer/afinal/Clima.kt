d08bcbf4c4ba608a9a6794dee38f42f5c52df0c0
Eliezer Ochoa <eliezerochoa20@gmail.com>
2020-12-12 18:49:30 +0100

    Modificacion del codigo en el main de clima

--- FINAL/app/src/main/java/com/eliezer/afinal/Clima.kt
+++ FINAL/app/src/main/java/com/eliezer/afinal/Clima.kt
@@ -10,27 +10,27 @@ import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
+import java.lang.Exception
 
 class Clima : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clima)
 
-
-
-
         if (Network.hayRed(this))
         {
+            Toast.makeText(this,"hay red",Toast.LENGTH_LONG).show()
             solicitudHTTPVolley("https://api.openweathermap.org/data/2.5/weather?id=3616253&appid=d3082f4bab5291b3ce8040e32600636a&lang=es&units=metric")
-        }
-        else {
-            Toast.makeText(this, "No hay red", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this,"No hay red",Toast.LENGTH_LONG).show()
         }
     }
-    fun solicitudHTTPVolley(Url:String)
+
+    fun solicitudHTTPVolley(url:String)
     {
+
         val queue = Volley.newRequestQueue(this)
-        val solicitud = StringRequest(Request.Method.GET,Url,Response.Listener<String> {
+        val solicitud = StringRequest(Request.Method.GET,url,Response.Listener<String> {
                 response ->
             try {
                 Log.d("ResultadoVolley",response)
@@ -38,16 +38,16 @@ class Clima : AppCompatActivity() {
                 val ciudad = gson.fromJson(response, Ciudad::class.java)
                 val tvGrado: TextView = findViewById(R.id.tvgrado)
                 val tvCiuda: TextView = findViewById(R.id.tvciudad)
-                val tvDescripcion: TextView = findViewById(R.id.tvciudad)
-                tvGrado.text = ciudad.main!!.Temp.toString()
+                val tvDescripcion: TextView = findViewById(R.id.tvdescripcion)
+                tvGrado.text = ciudad.main!!.temp.toString() + "°"
                 tvCiuda.text = ciudad.name
-                tvDescripcion.text =ciudad.weather!!.get(0).descripcion
+                tvDescripcion.text = ciudad.weather!!.get(0).descripcion
             } catch (e:Exception){
-             
+
             }
         }, Response.ErrorListener {
-            Log.e("ErrorVolley","ErrorExtra") }
-        )
+            Log.e("ErrorVolley","ERROR EN LA CONSULTA")
+        })
         queue.add(solicitud)
     }
 }
