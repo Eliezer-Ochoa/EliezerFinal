fcf07ac254847fca3b4b9a191c3cffbb322b6fba
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-12-12 21:28:28 -0600

    Cree el nombre de cada campo de activity agregar y las propiedades correspondiente

--- FINAL/app/src/main/java/com/eliezer/afinal/Registro.kt
+++ FINAL/app/src/main/java/com/eliezer/afinal/Registro.kt
@@ -1,14 +1,35 @@
 package com.eliezer.afinal
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
 
 class Registro : AppCompatActivity() {
+
+    private lateinit var txtName: EditText
+    private lateinit var txtLastName: EditText
+    private lateinit var txtEmail: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var  progressBar: ProgressDialog
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private lateinit var auth: FirebaseAuth
+
+    //global variables
+    private var firstName by Delegates.notNull<String>()
+    private var lastName by Delegates.notNull<String>()
+    private var email by Delegates.notNull<String>()
+    private var password by Delegates.notNull<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
+        initialice()
     }
 
     fun lanzarClima(view: View) {
