91f1b6568084c106fba3b3ed0a7e9f82c900e717
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-12-13 19:53:54 -0600

    Cree la clase Equipos para declarar las variables que estre ocupando poara la base de datos

---
+++ FINAL/app/src/main/java/com/eliezer/afinal/Equipos.kt
@@ -0,0 +1,29 @@
+package com.eliezer.afinal
+
+class Equipos(codigo:String, nombre:String, marca:String, modelo:String, serie:String, precio:String, estado:String, fechacompra:String, cantidad:String) {
+
+    private var codigo: String? = null;
+    private var nombre: String? = null;
+    private var marca: String? = null;
+    private var modelo: String? = null;
+    private var serie: String? = null;
+    private var precio: String? = null;
+    private var estado: String? = null;
+    private var fechacompra: String? = null;
+    private var cantidad: String? = null;
+
+    init{
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.serie = serie;
+        this.precio = precio;
+        this.estado = estado;
+        this.fechacompra = fechacompra;
+        this.cantidad = cantidad;
+    }
+
+
+
+}
