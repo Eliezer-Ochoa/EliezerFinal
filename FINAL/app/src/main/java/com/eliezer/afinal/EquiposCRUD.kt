49cd44d5d2f002934db81e0d1b969d34942bc745
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-12-13 22:31:46 -0600

    Terminando segunda función de getEquipos

--- FINAL/app/src/main/java/com/eliezer/afinal/EquiposCRUD.kt
+++ FINAL/app/src/main/java/com/eliezer/afinal/EquiposCRUD.kt
@@ -2,21 +2,24 @@ package com.eliezer.afinal
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 
 class EquiposCRUD(context: Context) {
 
-    private var helper:DataBaseHelper? = null
+    private var helper: DataBaseHelper? = null
+
     init {
-         helper = DataBaseHelper(context)
+        helper = DataBaseHelper(context)
     }
 
     //crear nuevo equipo
-    fun newEquipos(item:Equipos){
+    fun newEquipos(item: Equipos) {
         //creación de una neuva BD
         //Definiendo el obejeto de BD que se referncia a helper, ejcuta exectSQL DatBase Helper
         //Abrir BD modo escritura
-        val db: SQLiteDatabase = helper?.writableDatabase!!  //writableDatabasehabilitar BD para poder escribir en ella
+        val db: SQLiteDatabase =
+            helper?.writableDatabase!!  //writableDatabasehabilitar BD para poder escribir en ella
 
         //Llenando los datos que queremos insertar en nuestra tabla
         // Clase que me permite empexzar agrupar información para poder mapiarla con mi tabla
@@ -38,7 +41,61 @@ class EquiposCRUD(context: Context) {
         //Cerrando conexión
         db.close()
 
+    }
 
+    //Traera todos los elementospara mostrar en la interfazde la aplicación
+    //Generar consulta a nuestra BD
+        fun getEquipos(): ArrayList<Equipos> {
+        //definir nuestra BD
+
+        val items: ArrayList<Equipos> = ArrayList()
+        //Abrir BD en modo lectura
+        val db: SQLiteDatabase = helper?.readableDatabase!!
+
+        //Especuifuicar las columnas que quiero consultar
+        val columnas = arrayOf(
+            EquiposContract.Companion.Entrada.COLUMNA_ID,
+            EquiposContract.Companion.Entrada.COLUMNA_NOMBRE,
+            EquiposContract.Companion.Entrada.COLUMNA_MARCA,
+            EquiposContract.Companion.Entrada.COLUMNA_MODELO,
+            EquiposContract.Companion.Entrada.COLUMNA_SERIE,
+            EquiposContract.Companion.Entrada.COLUMNA_PRECIO,
+            EquiposContract.Companion.Entrada.COLUMNA_ESTADO,
+            EquiposContract.Companion.Entrada.COLUMNA_FECHACOMPRA,
+            EquiposContract.Companion.Entrada.COLUMNA_CANTIDAD
+        )
+
+        //Crear un curso para recorrer la tabla
+        val c: Cursor = db.query(
+            EquiposContract.Companion.Entrada.NOMBRE_TABLA,
+            columnas,
+            null,
+            null,
+            null,
+            null,
+            null
+        )
+          //Hacer el recorrido en la tabla
+         while (c.moveToNext()) {
+             items.add(
+                 Equipos(
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_ID)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_NOMBRE)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_MARCA)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_MODELO)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_SERIE)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_PRECIO)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_ESTADO)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_FECHACOMPRA)),
+                     c.getString(c.getColumnIndexOrThrow(EquiposContract.Companion.Entrada.COLUMNA_CANTIDAD))
+
+                     ))
+
+         }
+             //Cerrar DataBase
+             db.close()
+
+             return items
+         }
 
     }
-}
