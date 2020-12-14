7071ed32bc10c7e8e35029497bfba9cd3ea88a83
FerlyGuti_0002 <ferlyquiroz17@gmail.com>
2020-12-13 21:08:04 -0600

    Creación y programación de clase EquipoCRUD

--- FINAL/app/src/main/java/com/eliezer/afinal/Equipos.kt
+++ FINAL/app/src/main/java/com/eliezer/afinal/Equipos.kt
@@ -2,15 +2,15 @@ package com.eliezer.afinal
 
 class Equipos(codigo:String, nombre:String, marca:String, modelo:String, serie:String, precio:String, estado:String, fechacompra:String, cantidad:String) {
 //Creación de clase para nuestros campos de tabla de la BD
-    private var codigo: String? = null;
-    private var nombre: String? = null;
-    private var marca: String? = null;
-    private var modelo: String? = null;
-    private var serie: String? = null;
-    private var precio: String? = null;
-    private var estado: String? = null;
-    private var fechacompra: String? = null;
-    private var cantidad: String? = null;
+    var codigo: String? = null;
+    var nombre: String? = null;
+    var marca: String? = null;
+    var modelo: String? = null;
+    var serie: String? = null;
+    var precio: String? = null;
+    var estado: String? = null;
+    var fechacompra: String? = null;
+    var cantidad: String? = null;
 
     init{
         this.codigo = codigo;
